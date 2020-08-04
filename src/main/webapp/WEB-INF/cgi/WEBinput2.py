3d0ee6de123b53093b05384c205e3dd6c626690c
probson <probson@aligntech.com>
2020-08-04 20:28:42 +0000

    updated WEBinput2.py with the correct python code using ()s

--- src/main/webapp/WEB-INF/cgi/WEBinput2.py
+++ src/main/webapp/WEB-INF/cgi/WEBinput2.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+
 import cgi
 form = cgi.FieldStorage()
 patname =  form.getvalue('pname')
@@ -6,12 +8,12 @@ docname = form.getvalue('dname')
 docID = form.getvalue('did')
 
 
-print "Content-type:text/html\r\n\r\n";
-print "<html>";
-print "<head>";
-print "<title>Hello - Second CGI Program</title>";
-print "</head>";
-print "<body>")
-print "<h2>Hello %s %s</h2>" % (patname, patID);
-print "</body>";
-print "</html>";
+print ("Content-type:text/html\r\n\r\n")
+print ("<html>")
+print ("<head>")
+print ("<title>Hello - Second CGI Program</title>")
+print ("</head>")
+print ("<body>")
+print ("(<h2>Hello %s, %s, %s, %s</h2>" % (patname, patID, docname, docID))
+print ("</body>")
+print ("</html>")
